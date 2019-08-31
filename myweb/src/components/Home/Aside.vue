e4dd09c48016304c5a9acfa6dff27c5c24135384
czh789dv <49721043+czh789dv@users.noreply.github.com>
2019-08-31 16:00:05 +0800

    1

---
+++ myweb/src/components/Home/Aside.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page">
+<el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="Transparent"
+      text-color="#000000"
+      active-text-color="#409EFF">
+      <el-menu-item index="1">
+        <i class="el-icon-user"></i>
+        <span slot="title">导航一</span>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      
+      <el-menu-item index="3">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+   methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+}
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+
+</style>
---
+++ myweb/src/components/Home/HomeTitle.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="page">
+
+    <el-row :gutter="20">
+      <el-col :span="4">logo
+      </el-col>
+      <el-col :span="16">陈子豪
+      </el-col>
+      <el-col :span="4">退出
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+</style>
